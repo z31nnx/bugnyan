7afc4e8d54076fa733679a72a1babcd363c85dd8
z31nnx <zeistnn@gmail.com>
2025-06-11 18:10:36 -1000

    bug: added iam stack and security groups

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -13,3 +13,20 @@ module "vpc-stack" {
   cidr_block  = var.cidr_block
   global_tags = local.global_tags
 }
+
+module "iam-stack" {
+  source      = "../../modules/iam-stack"
+  role_name   = var.role_name
+  global_tags = local.global_tags
+}
+
+module "sg-stack" {
+  source                   = "../../modules/sg-stack"
+  bugnyan_web_alb_sg_name  = var.bugnyan_web_alb_sg_name
+  bugnyan_web_asg_sg_name  = var.bugnyan_web_asg_sg_name
+  bugnyan_app_alb_sg_name  = var.bugnyan_app_alb_sg_name
+  bugnyan_app_asg_sg_name  = var.bugnyan_app_asg_sg_name
+  bugnyan_database_sg_name = var.bugnyan_database_sg_name
+  bugnyan_vpc_id           = module.vpc-stack.bugnyan_vpc_id
+  global_tags              = local.global_tags
+}
--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -7,3 +7,13 @@ variable "Terraform" {}
 # VPC
 variable "vpc_name" {}
 variable "cidr_block" {}
+
+# IAM 
+variable "role_name" {}
+
+# Security Groups 
+variable "bugnyan_web_asg_sg_name" {}
+variable "bugnyan_web_alb_sg_name" {}
+variable "bugnyan_app_asg_sg_name" {}
+variable "bugnyan_app_alb_sg_name" {}
+variable "bugnyan_database_sg_name" {}
