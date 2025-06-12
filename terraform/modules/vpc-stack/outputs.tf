e7f1a792d2f83d5beb19135cad9a3f5bd2308fa2
z31nnx <zeistnn@gmail.com>
2025-06-11 14:25:54 -1000

    feat/vpc: added vpc module stack

---
+++ terraform/modules/vpc-stack/outputs.tf
@@ -0,0 +1,19 @@
+output "nyanbug_vpc_id" {
+  value = aws_vpc.bugnyan.id
+}
+
+output "nyanbug_public_subnets_ids" {
+  value = [for s in aws_subnet.bugnyan_public_subnets : s.id]
+}
+
+output "nyanbug_private_subnets_ids" {
+  value = [for s in aws_subnet.bugnyan_private_subnets : s.id]
+}
+
+output "bugnyan_igw_id" {
+  value = aws_internet_gateway.bugnyan_igw.id
+}
+
+output "bugnyan_natgw_id" {
+  value = aws_nat_gateway.bugnyan_natgw.id
+}
---
+++ terraform/modules/vpc-stack/variables.tf
@@ -0,0 +1,6 @@
+variable "global_tags" {
+  type = map(string)
+}
+
+variable "vpc_name" {}
+variable "cidr_block" {}
