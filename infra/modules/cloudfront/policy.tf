f8661d60e09a59969984de8d08c7a258b0c26732
z31nnx <196381025+z31nnx@users.noreply.github.com>
2025-09-08 13:34:12 -1000

    Deleted region

--- infra/modules/cloudfront/policy.tf
+++ infra/modules/cloudfront/policy.tf
@@ -4,7 +4,6 @@ data "aws_partition" "current" {}
 locals {
   account_id   = data.aws_caller_identity.current.account_id
   partition    = data.aws_partition.current.partition
-  region       = data.aws_region.current.name
   distribution = aws_cloudfront_distribution.web_distribution.id
 }
 
